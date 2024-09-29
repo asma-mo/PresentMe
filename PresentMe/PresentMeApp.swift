292c6dbe4c52dfe44d330a93135595d588811261
asma <Asma.mo@outlook.com>
2024-09-29 14:45:38 +0300

    Initial Commit

---
+++ PresentMe/PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp.swift
+//  PresentMe
+//
+//  Created by Asma on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PresentMeTests/PresentMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeTests.swift
+//  PresentMeTests
+//
+//  Created by Asma on 29/09/2024.
+//
+
+import Testing
+@testable import PresentMe
+
+struct PresentMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
